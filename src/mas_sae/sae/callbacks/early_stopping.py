272816029f0c621c1156ac3ae40b1e2229922ccb
Jschechter18 <joshua.d.schechter@gmail.com>
2026-09-16 16:20:04 -0400

    updated docstrings

--- src/mas_sae/sae/callbacks/early_stopping.py
+++ src/mas_sae/sae/callbacks/early_stopping.py
@@ -7,7 +7,22 @@ class EarlyStoppingCallback:
         self.best_loss = float('inf')
         self.counter = 0
 
-    def on_validation_end(self, val_loss):
+    def on_validation_end(self, val_loss: float) -> bool:
+        """Check if early stopping should be triggered based on the validation loss.
+
+        Early stopping is triggered if the validation loss does not improve for a number of consecutive epochs
+        defined by the patience parameter.
+
+        Parameters
+        ----------
+        val_loss : float
+            The validation loss for the current epoch.
+
+        Returns
+        -------
+        bool
+            True if early stopping should be triggered, False otherwise.
+        """
         if val_loss < self.best_loss - self.min_delta:
             self.best_loss = val_loss
             self.counter = 0
